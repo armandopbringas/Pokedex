c152dd14f7209220b5e7819fb8f39c3d18765b36
armanopbringas <bringas.armandop@gmail.com>
2022-06-23 00:37:52 -0500

    setup pokemon API calling

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useEffect } from 'react'
+import { getPokemons } from '../../api/getPokemons'
 import PokemonList from '../../components/PokemonList/PokemonList';
 import Searcher from '../../components/Searcher';
 import './styles.css';
 
 function Home() {
+  useEffect(() => {
+		getPokemons().then(response => console.log(response))
+	}, [])
+
   return (
     <div className='Home'>
       <Searcher />
